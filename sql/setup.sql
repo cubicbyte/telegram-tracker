24c8dc5f5db880b7debcb3572a685126c7594326
Bohdan Marukhnenko <bmaruhnenko@gmail.com>
2023-05-31 18:50:03 +0300

    User updates are now registered

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,5 @@
 
+# User status updates
 CREATE TABLE updates (
     id bigint,
     state bool,
@@ -10,6 +11,20 @@ CREATE INDEX updates_time ON updates(time);
 
 
 
+# User updates
+CREATE TABLE user_updates (
+    id bigint,
+    username varchar(32),
+    first_name varchar(255),
+    last_name varchar(255),
+    phone_number varchar(20)
+);
+
+CREATE INDEX user_updates_id ON user_updates(id);
+
+
+
+# Users
 CREATE TABLE users (
     id bigint PRIMARY KEY,
     status_online bool,
@@ -23,6 +38,7 @@ CREATE TABLE users (
 
 
 
+# Update user status to offline if it has expired
 CREATE PROCEDURE updateUser (
     user_id bigint
 )
@@ -33,43 +49,44 @@ BEGIN
     FROM users
     WHERE id = user_id;
 
-    IF @status = 1 THEN
-        IF @expires < NOW() THEN
-            UPDATE users
-            SET status_online = 0,
-                status_time = @expires,
-                status_expires = NULL
-            WHERE id = user_id;
+    # If online status expired, set to offline
+    IF @status = 1 AND @expires < NOW() THEN
+        UPDATE users
+        SET status_online = 0,
+            status_time = @expires,
+            status_expires = NULL
+        WHERE id = user_id;
 
-            INSERT INTO updates (
-                id,
-                state,
-                time
-            )
-            VALUES (
-                user_id,
-                0,
-                @expires
-            );
-        END IF;
+        INSERT INTO updates (
+            id,
+            state,
+            time
+        )
+        VALUES (
+            user_id,
+            0,
+            @expires
+        );
     END IF;
 
 END;
 
 
 
+# Main procedure for handling user updates
 CREATE PROCEDURE handleUserUpdate (
     user_id bigint,
     state bool,
     time timestamp,
     expires timestamp,
-    username varchar(255),
+    username varchar(32),
     first_name varchar(255),
     last_name varchar(255),
     phone_number varchar(20)
 )
 BEGIN
 
+    # If user does not exist, create new user
     IF NOT EXISTS(SELECT id FROM users WHERE id = user_id) THEN
 
         INSERT INTO users (
@@ -104,8 +121,10 @@ BEGIN
             time
         );
 
+    # If user exists, handle update
     ELSE
 
+        # Update expired status
         CALL updateUser(user_id);
 
         SELECT status_online
@@ -115,6 +134,7 @@ BEGIN
 
         IF @status = state THEN
             IF state = 1 THEN
+                # Renew status expiration time
                 UPDATE users
                 SET status_expires = expires
                 WHERE id = user_id;
@@ -149,6 +169,52 @@ BEGIN
             );
         END IF;
 
+    END IF;
+
+    CALL saveUserUpdate(user_id);
+
+END;
+
+
+
+# Save user data updates (name, phone number, ...)
+CREATE PROCEDURE saveUserUpdate (
+    user_id bigint
+)
+BEGIN
+
+    # Get new user data
+    SELECT username, first_name, last_name, phone_number
+    INTO @username, @first_name, @last_name, @phone_number
+    FROM users
+    WHERE id = user_id;
+
+    # Get old user data
+    SELECT username, first_name, last_name, phone_number
+    INTO @old_username, @old_first_name, @old_last_name, @old_phone_number
+    FROM user_updates
+    WHERE id = user_id;
+
+    # Save user update if some user data field has changed
+    IF NOT (@username <=> @old_username
+        AND @first_name <=> @old_first_name
+        AND @last_name <=> @old_last_name
+        AND @phone_number <=> @old_phone_number)
+    THEN
+        INSERT INTO user_updates (
+            id,
+            username,
+            first_name,
+            last_name,
+            phone_number
+        )
+        VALUES (
+            user_id,
+            @username,
+            @first_name,
+            @last_name,
+            @phone_number
+        );
     END IF;
 
 END;
